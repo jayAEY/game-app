d66e9114f6c505ab335d368381f292e269691408
jayAEY <106685702+jayAEY@users.noreply.github.com>
2023-12-25 14:20:55 -0800

    continued work on backlog feature

--- src/backlog.tsx
+++ src/backlog.tsx
@@ -28,6 +28,105 @@ const Backlog = (props) => {
     "Metroid Prime",
   ];
 
+  // let tasks = [];
+  // let newTaskHTML = "";
+
+  // function renderTasks() {
+  //   if (localStorage.getItem("tasks")) {
+  //     tasks = localStorage.getItem("tasks").split(",");
+  //   }
+  //   if (tasks.length > 0) {
+  //     todosection.style.display = "block";
+  //   }
+  //   tasksHTML();
+  // }
+
+  // window.onload = renderTasks();
+
+  // function tasksHTML() {
+  //   newTaskHTML = "";
+  //   tasks.forEach((task) => {
+  //     let addHTML = `<li class="task" id="${task}">
+  //                      ${task}
+  //                     <input
+  //                     class="taskcheckbox"
+  //                     type="checkbox"
+  //                     />
+  //                     </li>`;
+  //     newTaskHTML += addHTML;
+  //   });
+  //   taskList.innerHTML = newTaskHTML;
+  //   taskList.style.display = "flex";
+  // }
+
+  // function addTask(btn) {
+  //   let task = btn.previousElementSibling.value.trim();
+  //   todosection.style.display = "block";
+  //   tasks.push(task);
+  //   localStorage.setItem("tasks", tasks);
+  //   taskList.style.display = "flex";
+  //   searchList.style.display = "none";
+  //   renderTasks();
+  // }
+
+  // function searchTask(btn) {
+  //   let search = btn.previousElementSibling.value.trim();
+  //   if (search === "") {
+  //     taskList.style.display = "flex";
+  //     searchList.style.display = "none";
+  //   } else {
+  //     let searchHTML = "";
+  //     taskList.childNodes.forEach((task) => {
+  //       if (task.innerText.includes(search)) {
+  //         searchHTML += task.outerHTML;
+  //       }
+  //     });
+  //     taskList.style.display = "none";
+  //     searchList.style.display = "flex";
+  //     searchList.innerHTML = searchHTML;
+  //   }
+  // }
+
+  // function deleteTasks() {
+  //   let taskListArr = Array.from(taskList.children);
+  //   let searchListArr = Array.from(searchList.children);
+
+  //   let newTasks = [];
+
+  //   for (let i = 0; i < taskListArr.length; i++) {
+  //     if (!taskListArr[i].lastElementChild.checked) {
+  //       newTasks.push(taskListArr[i].innerText.trim());
+  //     }
+  //   }
+
+  //   if (searchList.style.display == "flex") {
+  //     for (let i = 0; i < searchListArr.length; i++) {
+  //       if (searchListArr[i].lastElementChild.checked) {
+  //         let checkedElem = searchListArr[i].innerText.trim();
+  //         newTasks.splice(newTasks.indexOf(checkedElem), 1);
+  //       }
+  //     }
+  //   }
+
+  //   tasks = newTasks;
+  //   if (tasks.length == 0) todosection.style.display = "none";
+  //   localStorage.setItem("tasks", tasks);
+  //   tasksHTML();
+  //   searchList.style.display = "none";
+  // }
+
+  // addButton.addEventListener("click", (e) => {
+  //   if (taskInput.value.indexOf(",") !== -1 || taskInput.value.trim() === "") {
+  //     taskInput.setCustomValidity("Please fill out this field (No commas)");
+  //   } else {
+  //     addTask(e.target);
+  //   }
+  // });
+  // searchButton.addEventListener("click", (e) => searchTask(e.target));
+  // deleteButton.addEventListener("click", (e) => deleteTasks());
+
+  // window.onload = renderTasks();
+
   function selectGame(elem) {
     if (elem.children[0]) {
       elem.children[0].checked == true
